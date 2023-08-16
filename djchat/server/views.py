1f8947e58636857558c6b577ce18140974bea36e
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2023-08-16 20:18:01 +1000

    filtering servers and return annotation

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from django.db.models import Count
 
 from .models import Server
 from .serializer import ServerSerializer
@@ -16,7 +16,8 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
-        by_serverid = request.query_params.get("by_serverid") 
+        by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if by_user or by_serverid and not request.user.is_authenticated:
             raise AuthenticationFailed()
@@ -27,6 +28,9 @@ class ServerListViewSet(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
         if qty:
             self.queryset = self.queryset[:int(qty)]    
@@ -39,6 +43,6 @@ class ServerListViewSet(viewsets.ViewSet):
             except ValueError:
                 raise ValidationError(detail="Server value error")
         
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(self.queryset, many=True, context={"num_members": with_num_members})
         return Response(serializer.data)
     
