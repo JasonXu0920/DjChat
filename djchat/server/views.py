40506511de81ba653604847a818e66c2c2f651e8
Jason Xu <97411709+JasonXu0920@users.noreply.github.com>
2023-08-17 20:40:41 +1000

    stringdocs

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from django.db.models import Count
+from .schema import server_list_docs
 
 from .models import Server
 from .serializer import ServerSerializer
@@ -12,22 +13,39 @@ from .serializer import ServerSerializer
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
+        """
+        List servers based on various query parameters.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+
+        Returns:
+            Response: A response containing a list of serialized server data.
+
+        Raises:
+            AuthenticationFailed: If the user is not authenticated and 'by_user' or 'by_serverid' is specified.
+            ValidationError: If server filtering by 'by_serverid' encounters errors, or if a specified server ID is not found.      
+        """
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
         with_num_members = request.query_params.get("with_num_members") == "true"
 
-        if by_user or by_serverid and not request.user.is_authenticated:
-            raise AuthenticationFailed()
+        # if by_user or by_serverid and not request.user.is_authenticated:
+        #    raise AuthenticationFailed()
 
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
         if by_user:
-            user_id = request.user.id
-            self.queryset = self.queryset.filter(member=user_id)
+            if by_user and request.user.is_authenticated:
+                user_id = request.user.id
+                self.queryset = self.queryset.filter(member=user_id)
+            else:
+                raise AuthenticationFailed()
 
         if with_num_members:
             self.queryset = self.queryset.annotate(num_members=Count("member"))
@@ -36,6 +54,9 @@ class ServerListViewSet(viewsets.ViewSet):
             self.queryset = self.queryset[:int(qty)]    
         
         if by_serverid:
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+            
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
